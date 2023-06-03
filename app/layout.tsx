d8bce95cff8005f7a7c927c4f5980979904f9c6f
在虎 <caiths@icloud.com>
2023-06-03 21:43:26 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "小商助手",
-  description: "使用openai内核的的智能助手。",
+  title: "题解助手",
+  description: "你的私人题解助手。",
   appleWebApp: {
-    title: "小商助手",
+    title: "开始搜索",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
